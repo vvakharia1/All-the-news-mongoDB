f928e8a957f8b8ddeeb7f36125bebe3e38479f0d
Vidhi Vaks <vidhi.vakharia93@gmail.com>
2019-09-04 21:06:18 -0400

    added bootstrap layout and css

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-// Depenencies 
+// Depenencies
 var express = require("express");
 var mongojs = require("mongojs");
 var mongoose = require("mongoose");
 
-// Scraping Tools 
+// Scraping Tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
-var axios = require("axios"); // make an http request with the call 
-var cheerio = require("cheerio"); // web scraper when a site doesn't have an API or when you don't want to go 
-// through the process of connecting to the api and making calls 
+var axios = require("axios"); // make an http request with the call
+var cheerio = require("cheerio"); // web scraper when a site doesn't have an API or when you don't want to go
+// through the process of connecting to the api and making calls
+
+// Mongoose to MongoDB connection
+var MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
+/**
+ * 
+ * pick a new site
+ * read html for where stories are contained
+ * use cheerio to load that page via url
+ * search for the element that contains stories
+ * 
+ */
